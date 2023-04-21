55cfa214eea3abf97c369929ec942369c5d867cd
Reinan Khevne <reinanbomfin@gmail.com>
2023-04-20 22:58:32 -0300

    funcionando

--- index.py
+++ index.py
@@ -4,5 +4,18 @@ print('=-' * 20)
 print('Jokepô')
 print('=-' * 20)
 
-jog1 = str(input('Pedra, papel ou tesoura?? '))
-jog2 = randint(1, 3)
+jog = int(input('Pedra(1), tesoura(2) ou papel(3) '))
+pc = randint(1, 3)
+
+# possibilidades de vitória
+
+if jog == 1 and pc == 2 or jog == 2 and pc == 3 or jog == 3 and pc == 1:
+    print('PARABÉNS! Você venceu eu joguei {}'.format(pc))
+
+# possibilidades de empate
+
+elif jog == 1 and pc == 1 or jog == 2 and pc == 2 or jog == 3 and pc == 3:
+    print('Jogamos iguais! Vamos de novo.')
+
+else:
+    print('NÃO FOI DESSA VEZ! Te venci com {}'.format(pc))
